80e34f1aeb8e1e169996fc0001cbe79903700de6
silva05 <71233532+silva05@users.noreply.github.com>
2022-03-28 20:14:52 -0500

    Update select.sql

--- SELECT/select.sql
+++ SELECT/select.sql
@@ -1,41 +1,32 @@
--- SELECT
--- Puede recibir constantes, expresiones, funciones, campos de tablas.
+-- SELECT: Puede recibir constantes, expresiones, funciones, campos de tablas.
 
--- Constantes
-SELECT 'Hola mundo';
-SELECT 8;
+	-- Constantes
+		SELECT 'Hola mundo';
+		SELECT 8;
 
--- Expresiones
-SELECT 2*2+2/2;
+	-- Expresiones
+		SELECT 2*2+2/2;
 
--- Cast
-SELECT 8::numeric/23::numeric;
+	-- Cast
+		SELECT CAST(12/12 AS CHAR);
+		SELECT CAST('2021-06-13' AS date);
 
--- Cast no válido
-SELECT 'Hola'::int;
+	-- Funciones
+		SELECT now();
+		SELECT upper('Hola Mundo');
+		SELECT lower('Hola Mundo');
 
--- Cast válido de String a Date
-SELECT '2021-06-13'::date;
+	-- Campos de Tablas
+		SELECT * FROM students;
+		SELECT id, nombre, activo FROM students;
 
+	-- Uniendo lo anterior
+		SELECT id, upper(nombre), CAST(2+4 AS CHAR), 'Estudiante' FROM students;
 
--- Funciones
-SELECT now();
-SELECT upper('Hola Mundo');
-SELECT lower('Hola Mundo');
+	-- Alias (Poner nombre a las columnas)
+		SELECT id, upper(nombre) AS nombre_mayus,
+			   CAST(2+4 AS CHAR) AS expresion,
+			   'Estudiante' as tipo_persona
+		FROM students;
 
--- Campos de Tablas
-SELECT * FROM students;
-SELECT id, first_name, is_active
-FROM students;
-
--- Uniendo lo anterior
-SELECT id, upper(first_name), (2+4)::numeric/3::numeric, 'Estudiante'
-FROM students;
-
--- Alias
-SELECT id, upper(first_name) AS first_name,
-       (2+4)::numeric/3::numeric AS expresion,
-       'Estudiante' as tipo_persona
-FROM students;
-
--- Le llamamos proyección a todos los campos que queremos que devuelva una consulta SQL
+	-- Le llamamos proyección a todos los campos que queremos que devuelva una consulta SQL
